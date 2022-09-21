3f9ac194d2536ad55cce25fc86fde1dfa81e4605
Your NameNitroceliulioze <aksionova.diana@gmail.com>
2022-09-21 17:48:55 +0300

    add settings page and footer margint-top

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -2,6 +2,8 @@ import CategoriesList from "./components/categories/CategoriesList";
 import HeroSection from "./components/mainpage/HeroSection";
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
+import Settings from "./pages/settingsPage/Settings";
+import './App.css';
 
 function App() {
   return (
@@ -10,8 +12,7 @@ function App() {
     <div className="App">
       <Navbar/>
       <div className="container main">
-        <HeroSection />
-        <CategoriesList />
+        <Settings/>
       </div>
       <Footer/>
     </div>

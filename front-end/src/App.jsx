00caef0133a3a4ef5c1519111f4365ca97e0439e
Giedre Liekiene <giedre.valanciute@gmail.com>
2022-09-22 01:53:34 +0300

    Add routes

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,27 +1,33 @@
-import CategoriesList from "./components/categories/CategoriesList";
-import HeroSection from "./components/mainpage/HeroSection";
-import QuoteSection from "./components/mainpage/QuoteSection";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
-import ContactUs from "./pages/contactUsPage/ContactUs";
+
 import './App.css';
+
+import HomePage from "./pages/homePage/HomePage"; 
+import RecipesPage from "./pages/recipesPage/RecipesPage";
+import ContactUsPage from "./pages/contactUsPage/ContactUs";
+import SettingsPage from "./pages/settingsPage/Settings";
 
 function App() {
   return (
-    <>
-    
-    <div className="App">
+    <Router>
       <Navbar/>
       <div className="container main">
-        <HeroSection/>
-        <QuoteSection />
-        <CategoriesList/>
-        
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/recipes" element={<RecipesPage />} />
+          <Route path="/contactus" element={<ContactUsPage />} />
+          <Route path="/settings" element={<SettingsPage />} />
+        </Routes>
       </div>
       <Footer/>
-    </div>
-    
-    </>
+    </Router>
     
   );
 }
---
+++ front-end/src/pages/homePage/HomePage.jsx
@@ -0,0 +1,15 @@
+import HeroSection from '../../components/mainpage/HeroSection';
+import QuoteSection from '../../components/mainpage/QuoteSection';
+import CategoriesSection from '../../components/mainpage/CategoriesSection';
+
+
+export default function HomePage() {
+  return (
+    <div>
+         <HeroSection/>
+         <QuoteSection />
+         <CategoriesSection />
+    </div>
+  )
+}
+

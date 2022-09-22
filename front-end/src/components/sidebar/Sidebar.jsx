b8b97d04112601a430ea783590201a21f6f85d72
Your NameNitroceliulioze <aksionova.diana@gmail.com>
2022-09-22 13:48:48 +0300

    fix contacts in dark, add chefs link in nav

--- front-end/src/components/sidebar/Sidebar.jsx
+++ front-end/src/components/sidebar/Sidebar.jsx
@@ -6,6 +6,7 @@ const Sidebar = ({ links, close }) => {
   const location = useLocation();
   return (
     <div className="sidebar" onClick={close}>
+      <Link to="/" className='logo'>Yum<span>my Coo</span>king</Link>
       {links.map((link) => (
         <Link to={link.path} className={location.pathname == link.path ? "sidebar-link active" : "sidebar-link" } key={link.name}>
             <FontAwesomeIcon icon={link.icon}/>

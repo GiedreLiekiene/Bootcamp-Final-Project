c722f8db905006b10621aff1e5cdfb4f3dcd15a6
Your NameNitroceliulioze <aksionova.diana@gmail.com>
2022-09-22 22:39:44 +0300

    add login and signup page

--- front-end/src/pages/logInPage/LogInPage.jsx
+++ front-end/src/pages/logInPage/LogInPage.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import './login.css'
 
 const LogInPage = () => {
   return (
-    <div>LogInPage</div>
+    <div className='login'>
+      <div className='login-wrapper'>
+        <div className='login-main'>
+          <h1>Welcome Back</h1>
+          <div className='login-input'>
+            <label>Username</label>
+            <input type="text" placeholder='Enter your username'></input>
+            <label>Password</label>
+            <input type="password" placeholder='Enter your password'></input>
+            <button className='login-btn'>Login</button>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 

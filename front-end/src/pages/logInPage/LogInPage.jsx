01992b858a0b935af90c821fae420fdd46f02af7
Your NameNitroceliulioze <aksionova.diana@gmail.com>
2022-09-22 23:54:06 +0300

    Add reusable login and sign up

--- front-end/src/pages/logInPage/LogInPage.jsx
+++ front-end/src/pages/logInPage/LogInPage.jsx
@@ -1,3 +1,4 @@
+import InputForm from '../../components/inputForm/InputForm'
 import './login.css'
 
 const LogInPage = () => {
@@ -6,13 +7,7 @@ const LogInPage = () => {
       <div className='login-wrapper'>
         <div className='login-main'>
           <h1>Welcome Back</h1>
-          <div className='login-input'>
-            <label>Username</label>
-            <input type="text" placeholder='Enter your username'></input>
-            <label>Password</label>
-            <input type="password" placeholder='Enter your password'></input>
-            <button className='login-btn'>Login</button>
-          </div>
+          <InputForm fullName='Username' email='Email' passwordHash='Password' avatarUrl='Avatar URL' buttonText='Login'/>
         </div>
       </div>
     </div>

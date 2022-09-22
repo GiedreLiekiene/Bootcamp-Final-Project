5edf9a4d7e36e1641212103633af53c2878a0b59
Giedre Liekiene <giedre.valanciute@gmail.com>
2022-09-22 16:45:24 +0300

    Add recipes card to RecipesPage

--- front-end/src/pages/recipesPage/RecipeCard.jsx
+++ front-end/src/pages/recipesPage/RecipeCard.jsx
@@ -1,12 +1,15 @@
 import "./recipeCard.css";
 import CustomImage from '../../components/CustomImage';
 
-const RecipeCard = () => {
+const RecipeCard = ({recipe}) => {
   return (
     <div className="recipe-card">
-        <CustomImage imgSrc="/img/gallery/img_1.jpg" pt="65%"/>
+        <CustomImage imgSrc={recipe.image} pt="65%"/>
         <div className="recipe-card-info">
-            <img src="/img/top-chefs/img_1.jpg" alt="" />
+            <img className="author-img" src={recipe.authorImg} alt="" />
+            <p className="recipe-title">{recipe.title}</p>
+            <p className="recipe-desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, molestias atque soluta autem tenetur ullam facilis voluptas dolore natus ad quisquam! Perferendis eligendi quasi optio blanditiis dignissimos aspernatur impedit voluptas.</p>
+            <a className="view-btn" href="#">VIEW RECIPE</a>
     </div>
     </div>
   )
--- front-end/src/pages/recipesPage/RecipesPage.jsx
+++ front-end/src/pages/recipesPage/RecipesPage.jsx
@@ -4,17 +4,57 @@ import CategoriesSection from '../../components/mainpage/CategoriesSection';
 import './recipesPage.css';
 
 const RecipesPage = () => {
- 
+  const recipes = [
+    {
+      title: "Green salad",
+      image: "/img/gallery/img_1.jpg",
+      authorImg: "/img/top-chefs/img_1.jpg",
+    },
+    {
+      title: "American pancakes",
+      image: "/img/gallery/img_2.jpg",
+      authorImg: "/img/top-chefs/img_2.jpg",
+    },
+    {
+      title: "Breakfast bowl",
+      image: "/img/gallery/img_3.jpg",
+      authorImg: "/img/top-chefs/img_3.jpg",
+    },
+    { 
+      title: "Panacotta",
+      image: "/img/gallery/img_4.jpg",
+      authorImg: "/img/top-chefs/img_4.jpg",
+    },
+    {
+      title: "Dinner",
+      image: "/img/gallery/img_5.jpg",
+      authorImg: "/img/top-chefs/img_5.jpg",
+    },
+    {
+      title: "Pumpkin soup",
+      image: "/img/gallery/img_6.jpg",
+      authorImg: "/img/top-chefs/img_6.jpg",
+    },
+    {
+      title: "Healthy",
+      image: "/img/gallery/img_7.jpg",
+      authorImg: "/img/top-chefs/img_7.jpg",
+    },
+    {
+      title: "Vegetarian",
+      image: "/img/gallery/img_8.jpg",
+      authorImg: "/img/top-chefs/img_8.jpg",
+    }
+  ].sort(() => Math.random() - 0.5);
+
   return (
     <>
     <PreviousSearches />
     <div className='recipes-container'>
-      <RecipeCard />
-      <RecipeCard />
-      <RecipeCard />
-      <RecipeCard />
-      <RecipeCard />
-      <RecipeCard />
+      {/* <RecipeCard /> */}
+      {recipes.map((recipe, index) => (
+        <RecipeCard key={index} recipe={recipe} />
+      ))}
     </div>
     <CategoriesSection />
     </>

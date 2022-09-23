1447e308d3f6bd0d259130398bd93ce90c3c07ea
Your NameNitroceliulioze <aksionova.diana@gmail.com>
2022-09-23 17:42:15 +0300

    fix input order in login made settingsPage smaller

--- front-end/src/pages/signUpPage/SignUpPage.jsx
+++ front-end/src/pages/signUpPage/SignUpPage.jsx
@@ -1,13 +1,16 @@
 import './signUpPage.css'
 import InputForm from '../../components/inputForm/InputForm'
 
+
 const SignUpPage = () => {
+
+
   return (
     <div className='signup'>
       <div className='signup-wrapper'>
         <div className='signup-main'>
           <h1>We Need Some Information</h1>
-          <InputForm fullName='Username' email='Email' passwordHash='Password' avatarUrl='Avatar URL' buttonText='Sign Up' isSignUp={true}/>
+          <InputForm fullName='Full Name' email='Email' passwordHash='Password' avatarUrl='Avatar URL' buttonText='Sign Up' isSignUp={true}/>
         </div>
       </div>
     </div>

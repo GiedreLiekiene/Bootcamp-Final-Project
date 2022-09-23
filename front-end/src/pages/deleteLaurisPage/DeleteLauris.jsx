c125f9833f4b06f2cf635c94a5e86e40d7e43847
Your NameNitroceliulioze <aksionova.diana@gmail.com>
2022-09-23 22:40:14 +0300

    add delete lauris feature

--- front-end/src/pages/deleteLaurisPage/DeleteLauris.jsx
+++ front-end/src/pages/deleteLaurisPage/DeleteLauris.jsx
@@ -3,14 +3,19 @@ import { useState } from 'react'
 
 const DeleteLauris = () => {
     const [deleteLauris, setDeleteLauris] = useState(true)
+    const [buttonText, setButtonText] = useState("YES!")
+    function delLauris () {
+      setDeleteLauris(!deleteLauris);
+      setButtonText(deleteLauris ? "BRING HIM BACK!" : "YES!")
+    }
   return (
     <div className='deleteLauris-container'>
         <div className='deleteLauris-wrapper'>
             <h1 className='deleteLauris-h1'>Do You Want to Delete Lauris?</h1>
-            <div className={deleteLauris ? 'deleteLauris-image-wrapper active' : 'deleteLauris-image-wrapper'}>
-                <img className='deleteLauris-image' src="https://images.unsplash.com/photo-1580128637456-8f5ee699d117?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=787&q=80"></img>
-            </div>
-            <button onClick={() => {setDeleteLauris(!deleteLauris)} } className="deleteLauris-button">YES!</button>
+           { deleteLauris ? <div className={deleteLauris ? 'deleteLauris-image-wrapper active' : 'deleteLauris-image-wrapper'}>
+                <img className='deleteLauris-image' src="https://images.unsplash.com/photo-1580128637456-8f5ee699d117?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=787&q=80" alt='lauris picture'></img>
+            </div>: null} 
+            <button onClick={() => {delLauris()} } className="deleteLauris-button">{buttonText}</button>
         </div>
     </div>
   )

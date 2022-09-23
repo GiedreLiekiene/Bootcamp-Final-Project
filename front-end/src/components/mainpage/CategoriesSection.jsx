23378e8e0d1cad5645de590f43c6bcc0fff6fc49
Giedre Liekiene <giedre.valanciute@gmail.com>
2022-09-23 20:02:21 +0300

    useMemo hook example

--- front-end/src/components/mainpage/CategoriesSection.jsx
+++ front-end/src/components/mainpage/CategoriesSection.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
+import { useSearchParams } from 'react-router-dom'
 import "./categoriesSection.css";
 
-const CategoryItem = ({photo, name, link}) => {
+const CategoryItem = ({photo, name, category}) => {
+  const [, setSearchParamters] = useSearchParams()
   return (
     <li>
         <a
             className="category-button"
-            href={link}
+            //href={link}
+            onClick={() => { setSearchParamters({"category": category}) }}
         >
             <img src={photo} alt="" />
             <span className="category-name">{name}</span>
@@ -20,11 +23,11 @@ const CategoriesList = () => {
     <div className="categories-list-container">
       <div className="categories-list">
         <ol>
-        <CategoryItem photo="/img/gallery/img_1.jpg" name="Breakfast" link="recipes?category=breakfast" key="1" />
-        <CategoryItem photo="/img/gallery/img_2.jpg" name="Lunch" link="recipes?category=lunch" key="2" />
-        <CategoryItem photo="/img/gallery/img_3.jpg" name="Dinner" link="recipes?category=dinner" key="3" />
-        <CategoryItem photo="/img/gallery/img_4.jpg" name="Vegetarian" link="recipes?category=vegetarian" key="4" />
-        <CategoryItem photo="/img/gallery/img_6.jpg" name="Dessert" link="recipes?category=dessert" key="6" />
+        <CategoryItem photo="/img/gallery/img_1.jpg" name="Breakfast" category="breakfast" key="1" />
+        <CategoryItem photo="/img/gallery/img_2.jpg" name="Lunch" category="lunch" key="2" />
+        <CategoryItem photo="/img/gallery/img_3.jpg" name="Dinner" category="dinner" key="3" />
+        <CategoryItem photo="/img/gallery/img_4.jpg" name="Vegetarian" category="vegetarian" key="4" />
+        <CategoryItem photo="/img/gallery/img_6.jpg" name="Dessert" category="dessert" key="6" />
         </ol>    
       </div>  
     </div>

d49e687af52e8f2a6c05fd815dddf2611e045c84
Giedre Liekiene <giedre.valanciute@gmail.com>
2022-09-23 20:01:44 +0300

    useMemohook example

--- front-end/src/pages/recipesPage/RecipesPage.jsx
+++ front-end/src/pages/recipesPage/RecipesPage.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useMemo } from 'react'
 import { useSearchParams } from 'react-router-dom'
 import PreviousSearches from '../../components/recipespage/PreviousSearches';
 import RecipeCard from './RecipeCard';
@@ -7,26 +7,43 @@ import './recipesPage.css';
 import { ServerUrl } from '../../config';
 
 const RecipesPage = () => {
+  console.log("RecipesPage - Rendered START")
   const [searchParams] = useSearchParams();
   const category = searchParams.get("category");
 
-  const [recipes, setRecipes] = useState([]);
-  useEffect(() => {
-    let apiUrl = `${ServerUrl}/recipes`
-    if (category) {
-      apiUrl += `/sections/${category}`
-    }
+  const [allRecipes, setAllRecipes] = useState([]);
 
+  useEffect(() => {
+    console.log("RecipesPage - Load - Start");
+    let apiUrl = `${ServerUrl}/recipes`
     fetch(apiUrl)
       .then((response) => response.json())
       .then((response) => {
-        console.log(response);
-        response=response.slice(0, 8)
-        response.sort(() => Math.random() - 0.5)
-        setRecipes(response);        
+        //console.log(response);        
+        console.log("RecipesPage - Loaded");
+
+        setAllRecipes(response);        
       })
       .catch((err) => console.error(err));
-  }, [category]);
+  //}, [category]);
+  }, []);
+
+  const recipes = useMemo(() => {   
+    let recipes = allRecipes;
+    console.log("RecipesPage - Filter");
+    if (category) {      
+      recipes = recipes.filter((recipe) => {
+        //console.log(recipe, recipe.sections)
+        return recipe.sections.includes(category)
+      });
+    }
+
+    recipes=recipes.slice(0, 8)
+    recipes.sort(() => Math.random() - 0.5)
+    return recipes;
+  }, [allRecipes, category])
+
+  console.log("RecipesPage - Rendered END")
 
   return (
     <>

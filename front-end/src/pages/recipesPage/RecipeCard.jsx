cba096faf1ffd8063702473ca98e4e718a3dd393
Giedre Liekiene <giedre.valanciute@gmail.com>
2022-09-23 13:04:35 +0300

    update chefs url

--- front-end/src/pages/recipesPage/RecipeCard.jsx
+++ front-end/src/pages/recipesPage/RecipeCard.jsx
@@ -7,7 +7,7 @@ const RecipeCard = ({recipe}) => {
     <div className="recipe-card">
         <CustomImage imgSrc={ServerUrl + recipe.imageUrl} pt="65%"/>
         <div className="recipe-card-info">
-            <img className="author-img" src={recipe.authorImg} alt="" />
+            <img className="author-img" src={ServerUrl + recipe.user.avatarUrl} alt="" />
             <p className="recipe-title">{recipe.title}</p>
             <p className="recipe-desc">{recipe.description.substr(0, 200)}...</p>
             <a className="view-btn" href={`${ServerUrl}/recipe?id=${recipe._id}`}>VIEW RECIPE</a>

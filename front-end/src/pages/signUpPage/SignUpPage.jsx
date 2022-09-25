1f28e193df102fff4d167a5c6da97042d5c326e4
Your NameNitroceliulioze <aksionova.diana@gmail.com>
2022-09-25 14:37:41 +0300

    Add p in submit, edit footer

--- front-end/src/pages/signUpPage/SignUpPage.jsx
+++ front-end/src/pages/signUpPage/SignUpPage.jsx
@@ -10,6 +10,7 @@ const SignUpPage = () => {
       <div className='signup-wrapper'>
         <div className='signup-main'>
           <h1>We Need Some Information</h1>
+          <p>Add your details to be able to upload your recipe! Maybe you are our next Top Chef? </p>
           <InputForm fullName='Full Name' email='Email' password='Password' avatarUrl='Avatar URL' buttonText='Sign Up' isSignUp={true}/>
         </div>
       </div>
